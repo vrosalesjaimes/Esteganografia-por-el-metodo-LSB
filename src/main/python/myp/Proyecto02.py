69bba06710f7001b4c29475d5bbaac82b2a26204
Raúl Bernal Núñez <raulbernal@ciencias.unam.mx>
2022-04-25 19:17:35 -0500

    Empezando modulo Proyecto02 encargado de leer argumentos

--- src/main/python/myp/Proyecto02.py
+++ src/main/python/myp/Proyecto02.py
@@ -1,1 +1,23 @@
-class Proyecto01:
+import sys
+
+def numero_argumentos_validos(arreglo_entrada):
+    if len(arreglo_entrada) == 4 | len(arreglo_entrada) == 5:
+        return True
+    else:
+        return False
+
+def bandera_h(arreglo_entrada):
+    if "-h" in sys.argv:
+        return True
+    else:
+        return False
+
+def bandera_u(arreglo_entrada):
+    if "-u" in sys.argv:
+        return True
+    else:
+        return False
+
+
+print(numero_argumentos_validos(sys.argv))
+print(len(sys.argv))
